f352cbd542b00b7d0bfc4f948b57cebb5839f695
Cillian G <cilliangoan221@gmail.com>
2021-12-10 15:34:26 +0000

    Add function to generate player order button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,7 @@
  * **/
 
 document.addEventListener("DOMContentLoaded", function(){
-    
-    
-    let nameAndColor = "";
-
-    for(i = 0; i < 9; i++) {
-        nameAndColor += `<div>
-                <label for="name${[i]}"></label>        
-                <input type="text" name="name" id="name${[i]}" required>
-                <label for id="colors${[i]}">Player color</label>
-            <select name="color" id="colors${[i]}">
-                <option value="orange">Orange🟠</option>
-                <option value="purple">Purple🟣</option>
-                <option value="black">Black⚫</option>
-                <option value="blue">Blue🔵</option>
-                <option value="yellow">Yellow🟡</option>
-                <option value="brown">Brown🟤</option>
-                <option value="white">White⚪</option>
-                <option value="green">Green🟢</option>
-                <option value="red">Red🔴</option>
-            </select>
-        </div>`; 
-    }
-    console.log(nameAndColor)
-    document.getElementById("player-details-form").innerHTML = nameAndColor
+    document.getElementById("number-selection-form").addEventListener("submit", numberSubmit)
 
     /**
      * The button on the welcome page that hides the welcome/explanation text
@@ -56,6 +33,38 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("generate-player-order").addEventListener("click", generatePlayerOrder())
 })
 
-function generatePlayerOrder(){
+
+function numberSubmit(event) {
+    event.preventDefault();
+
+    let numberOfPlayers = document.getElementById("number-selection-form").elements['number'].value;
+    console.log(numberOfPlayers)
+
+    let nameAndColor = "";
+
+    for(i = 0; i < numberOfPlayers; i++) {
+        nameAndColor += `<div>
+                <label for="name${[i]}"></label>        
+                <input type="text" name="name" id="name${[i]}" required>
+                <label for id="colors${[i]}">Player color</label>
+            <select name="color" id="colors${[i]}">
+                <option value="orange">Orange🟠</option>
+                <option value="purple">Purple🟣</option>
+                <option value="black">Black⚫</option>
+                <option value="blue">Blue🔵</option>
+                <option value="yellow">Yellow🟡</option>
+                <option value="brown">Brown🟤</option>
+                <option value="white">White⚪</option>
+                <option value="green">Green🟢</option>
+                <option value="red">Red🔴</option>
+            </select>
+        </div>`; 
+    }
+    console.log(nameAndColor)
+    document.getElementById("player-details-form").innerHTML = nameAndColor
+
+}
+
+function generatePlayerOrder() {
     console.log("generatePlayerOrder")
 }
